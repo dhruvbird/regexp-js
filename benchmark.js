a40fd8bce912f09f5548132ecc32d731d369f0e5
Dhruv Matani <dhruvbird@gmail.com>
2013-10-26 17:06:44 -0700

    Added benchmarks.

---
+++ benchmark.js
@@ -0,0 +1,31 @@
+var b = require('b');
+var re = require('./regexp.js');
+var util = require('util');
+
+var REs = ["(([^:]+)://)?([^:/]+)(:([0-9]+))?(/.*)",
+	   ".*(([^:]+)://)?([^:/]+)(:([0-9]+))?(/.*)",
+	   ".*usd [+-]?[0-9]+.[0-9][0-9]"
+	  ];
+var texts = [ "http://www.linux.com/",
+	      "http://www.thelinuxshow.com/main.php3true",
+	      "usd 1234.00",
+	      "he said she said he said no",
+	      "same same same",
+	      "{1: this is some more text - and some more and some more and even more this is some more text and some more and some more and even more this is some more text and some more and some more and even more this is some more text and some more and some more and even more this is some more text and some more and some more and even more this is some more text and some more and some more and even more this is some more text and some more and some more and even more this is some more text and some more and some more and even more this is some more text and some more and some more and even more this is some more text and some more and some more and even more this is some more text and some more and some more and even more this is some more text and some more and some more and even more this is some more text and some more and some more and even more this is some more text and some more and some more and even more this is some more text and some more and some more and even more this is some more text and some more and some more and even more this is some more text and some more and some more and even more this is some more text and some more and some more and even more this is some more text and some more and some more and even more this is some more text and some more and some more and even more this is some more text and some more and some more and even more this is some more text and some more and some more and even more this is some more text and some more and some more and even more this is some more text and some more and some more and even more this is some more text and some more and some more and even more this is some more text and some more and some more and even more this is some more text and some more and some more and even more this is some more text and some more and some more and even more this is some more text and some more and some more and even more this is some more text and some more and some more and even more this is some more text and some more and some more and even more this is some more text and some more and some more and even more this is some more text and some more and some more and even more this is some more text and some more and some more and even more this is some more text and some more and some more and even more this is some more text and some more and some more and even more this is some more text and some more and some more and even more this is some more text and some more and some more and even more this is some more text and some more and some more and even more this is some more text and some more and some more and even more this is some more text and some more and some more and even more this is some more text and some more and some more and even more at the end -}"
+	    ];
+
+REs.forEach(function(RE) {
+    var REnfa = new re.RegExpNFA(RE);
+    var nfa = REnfa.toNFA();
+    texts.forEach(function(text, textIdx) {
+	var niters = 100;
+	// console.log("RE:", RE, "text:", text);
+	var bmName = util.format('RE: %s matching string #%d',
+				 RE, textIdx + 1);
+	b(bmName).run(1, function(runId) {
+	    for (var i = 0; i < niters; ++i) {
+		re.search(text, nfa);
+	    }
+	});
+    });
+});
